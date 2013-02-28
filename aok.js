0beaa6a4598af337bbca16efe7572038817a0b53
Ryan Van Etten <ryanve@gmail.com>
2013-02-27 20:35:46 -0500

    Tighten parens.

--- aok.js
+++ aok.js
@@ -11,11 +11,11 @@
 , nomen: true, plusplus: true, regexp: true, undef: true, sloppy: true, stupid: true
 , sub: true, white: true, indent: 4, maxerr: 180 */
 
-(function (root, name, definition) {// github.com/umdjs/umd
-    if ( typeof module != 'undefined' && module['exports'] ) { 
+(function(root, name, definition) {// github.com/umdjs/umd
+    if (typeof module != 'undefined' && module['exports']) { 
         module['exports'] = definition(); // node / ender / common
     } else { root[name] = definition(); } // browser
-}(this, 'aok', function () {
+}(this, 'aok', function() {
     
     var win = window
       , doc = document
@@ -28,10 +28,10 @@
      * @constructor 
      * @param  {(Object=}  data
      */
-    function Aok (data) {
+    function Aok(data) {
         var k;
-        if ( null != data ) {
-            for ( k in data ) { 
+        if (null != data) {
+            for (k in data) { 
                 this[k] = data[k]; 
             }
             this['run']();
@@ -41,47 +41,56 @@
     /**
      * @param  {Object=}  data
      */
-    function aok (data) { 
+    function aok(data) { 
         return new Aok(data); 
     }
     
     aok.prototype = Aok.prototype = {
         'pass': 'Pass'
       , 'fail': 'Fail'
-      , 'run': function () {// run the test and trigger the handler
-            if ( !(this instanceof aok) ) { throw new TypeError; }
-            if ( typeof this['test'] == 'function' ) { this['test'] = this['test'](); }
+      , 'run': function() {
+            // run the test and trigger the handler
+            if (!(this instanceof aok)) {
+                throw new TypeError; 
+            }
+            if (typeof this['test'] == 'function') {
+                this['test'] = this['test'](); 
+            }
             this['test'] = !!this['test'];
             this['handler'] && this['handler']();
         }
-      , 'handler': function () {// default handler can be overridden:
-            var msg = this['test'] ? this['pass'] : this['fail'];
-            if ( typeof msg == 'string' ) { aok['log']('#' + this['id'] + ': ' + msg); } 
-            else if ( typeof msg == 'function' ) { msg.call(this); }
+      , 'handler': function() {
+            // default handler can be overridden
+            var msg = this[this['test'] ? 'pass' : 'fail'];
+            if (typeof msg == 'string') { 
+                aok['log']('#' + this['id'] + ': ' + msg); 
+            } else if (typeof msg == 'function') { 
+                msg.call(this); 
+            }
         }
      };
     
     /**
      * @param {string}  n   
      */
-    aok['id'] = function (n) {
+    aok['id'] = function(n) {
         return doc.getElementById(n);
     };
    
     // console methods:
-    (function (make) {
+    (function(make) {
         make('log');
         make('trace');
         make('info' , 1);
         make('warn' , 1);
         make('error', 1);
         make('trace', 1);
-    }(function (name, force) {
-        aok[name] = aok.prototype[name] = hasConsole && typeof console[name] == 'function' ? function () {
+    }(function(name, force) {
+        aok[name] = aok.prototype[name] = hasConsole && typeof console[name] == 'function' ? function() {
             console[name].apply(console, arguments); 
-        } : force ? function () {
+        } : force ? function() {
             alert(name + ': ' + join.call(arguments, ' ')); 
-        } : function () {};
+        } : function() {};
     }));
     
     return aok;
