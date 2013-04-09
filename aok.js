b968d67b6e4fed72fb7a2b55dfe4a65a1b3cfd35
Ryan Van Etten <ryanve@gmail.com>
2013-04-09 18:05:04 -0400

    Allow shorthand for passing test functions.

--- aok.js
+++ aok.js
@@ -3,7 +3,7 @@
  * @link        github.com/ryanve/aok
  * @license     MIT
  * @copyright   2013 Ryan Van Etten
- * @version     0.8.1
+ * @version     0.8.2
  */
 
 /*jslint browser: true, devel: true, node: true, passfail: false, bitwise: true
@@ -25,12 +25,14 @@
       
     /**
      * @constructor 
-     * @param  {Object=}  data
+     * @param  {Object|Function=}  data
      */
     function Aok(data) {
         var k;
         if (null != data) {
-            for (k in data) {
+            if (typeof data == 'function') {
+                this['test'] = data;
+            } else for (k in data) {
                 this[k] = data[k]; 
             }
             this['run']();
