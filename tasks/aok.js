0c7bcacb6c380c5dab0620339b18da1e0bea3a7b
Ryan Van Etten <ryanve@gmail.com>
2013-10-30 22:33:31 -0400

    Allow task to open html files.

--- tasks/aok.js
+++ tasks/aok.js
@@ -1,7 +1,16 @@
 module.exports = function(grunt) {
+    var web, htm, open, path = require('path');
+    try {
+        open = require(path.resolve('node_modules/open'));
+        web = /^https?\:\/\//i;
+        htm = /\.html?$/i;
+    } catch(e) { open = 0; }
+
     grunt.registerMultiTask('aok', function() {
-        [].concat(this.data).some(function load(id) {
-            Array.isArray(id) ? id.some(load) : null == id || require(this.resolve(id));
-        }, require('path'));
+        [].concat(this.data).some(function bulk(id) {
+            if (Array.isArray(id)) id.some(bulk);
+            else if (open && web.test(id)) open(id);
+            else (open && htm.test(id) ? open : require)(path.resolve(id));
+        });
     });
 };
